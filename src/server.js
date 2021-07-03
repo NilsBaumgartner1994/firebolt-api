fda169454b3a2ea8492fb7c2ef7663cda3ab4e32
Nils <nilsbaumgartner1994@gmail.com>
2021-07-03 15:56:05 +0200

    Added minimal import package

---
+++ src/server.js
@@ -0,0 +1,29 @@
+"use strict";
+
+/**
+ * Server class
+ *
+ * @class Server
+ */
+class Server {
+
+    /**
+     * Creates an instance of Server.
+     *
+     * @param {Object} options
+     *
+     * @memberof Server
+     */
+    constructor(options) {
+        console.log("Create a new Server isntance");
+    }
+
+    /**
+     * Start Server.
+     *
+     * @memberof Server
+     */
+    start() {
+        console.log("Hello new World");
+    }
+}
