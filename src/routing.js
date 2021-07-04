8fa3710c4ece061026a9909c8a9b57a7ac5c9080
Nils <nilsbaumgartner1994@gmail.com>
2021-07-04 16:40:48 +0200

    Added Coverage Tests

---
+++ src/routing.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/**
+ * Auth class
+ *
+ * @class Auth
+ */
+export default class Auth {
+
+    /**
+     * Creates an instance of Auth.
+     *
+     * @param {Object} options
+     *
+     * @memberof Auth
+     */
+    constructor(options) {
+        console.log("Create a new Auth instance");
+    }
+
+    //TODO implement auth adapters
+}
